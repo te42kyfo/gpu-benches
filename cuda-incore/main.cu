71ece1bdf228a3a72632ee0fd6b94d8599203d93
Dominik Ernst <dominik.ernst@fau.de>
2018-12-06 13:44:58 +0100

    cuda-incore: sqrt and div latency/throughpuout measurements

--- cuda-incore/main.cu
+++ cuda-incore/main.cu
@@ -1,6 +1,7 @@
 #include "../MeasurementSeries.hpp"
 #include "../dtime.hpp"
 #include "../gpu-error.h"
+#include "../gpu_clock.cuh"
 #include "../metrics.cuh"
 #include <iomanip>
 #include <iostream>
@@ -57,31 +58,68 @@ __global__ void dependent_FMA_seperated(T p, T *A, int iters) {
   }
 }
 
+template <typename T, int N, int M>
+__global__ void dependent_div_seperated(T p, T *A, int iters) {
+
+#pragma unroll(1)
+  for (int iter = 0; iter < iters; iter++) {
+    for (int m = 0; m < M; m++) {
+      T t = p + threadIdx.x + iter + m;
+
+      for (int n = 0; n < N; n++) {
+        t = 0.1 / (t + 0.2);
+      }
+
+      A[threadIdx.x + iter] = t;
+    }
+  }
+}
+
+template <typename T, int N, int M>
+__global__ void dependent_sqrt_seperated(T p, T *A, int iters) {
+
+#pragma unroll(1)
+  for (int iter = 0; iter < iters; iter++) {
+
+    for (int m = 0; m < M; m++) {
+      T t = p + threadIdx.x + iter + m;
+
+      for (int n = 0; n < N; n++) {
+        t = sqrt(t + 0.2);
+      }
+
+      A[threadIdx.x + iter] = t;
+    }
+  }
+}
+
 template <typename T, int N, int M> double measure(int warpCount) {
-  const int iters = 4000;
+  const int iters = 10000;
   const int blockSize = 32 * warpCount;
   const int blockCount = 1;
 
   MeasurementSeries time;
 
   T *dA;
-  GPU_ERROR(cudaMalloc(&dA, 1 * sizeof(T)));
-  initKernel<<<52, 256>>>(dA, 1);
+  GPU_ERROR(cudaMalloc(&dA, iters * 2 * sizeof(T)));
+  initKernel<<<52, 256>>>(dA, iters * 2);
   GPU_ERROR(cudaDeviceSynchronize());
 
-  dependent_FMA_mixed<T, N, M><<<blockCount, blockSize>>>((T)0.2, dA, iters);
+  dependent_div_seperated<T, N, M>
+      <<<blockCount, blockSize>>>((T)0.32, dA, iters);
 
   GPU_ERROR(cudaDeviceSynchronize());
   for (int i = 0; i < 1; i++) {
     double t1 = dtime();
-    dependent_FMA_mixed<T, N, M><<<blockCount, blockSize>>>((T)0.2, dA, iters);
+    dependent_div_seperated<T, N, M>
+        <<<blockCount, blockSize>>>((T)0.32, dA, iters);
     GPU_ERROR(cudaDeviceSynchronize());
     double t2 = dtime();
     time.add(t2 - t1);
   }
   cudaFree(dA);
 
-  double rcpThru = time.value() * 735 * 1e6 / N  / iters / warpCount;
+  double rcpThru = time.value() * 1380 * 1e6 / N / iters / warpCount;
   cout << setprecision(1) << fixed << typeid(T).name() << " " << setw(5) << N
        << " " << warpCount << " " << setw(5) << M << " "
        << " " << setw(5) << time.value() * 100 << " " << setw(5)
@@ -94,21 +132,21 @@ template <typename T>
 map<pair<int, int>, double> measureTabular(int maxWarpCount) {
 
   map<pair<int, int>, double> results;
-  for (int warpCount = 1; warpCount <= maxWarpCount; warpCount++) {
-    results[{warpCount, 1}] = measure<T, 512, 1>(warpCount);
-    results[{warpCount, 2}] = measure<T, 512, 2>(warpCount);
-    results[{warpCount, 3}] = measure<T, 512, 3>(warpCount);
-    results[{warpCount, 4}] = measure<T, 512, 4>(warpCount);
-    results[{warpCount, 5}] = measure<T, 512, 5>(warpCount);
-    results[{warpCount, 6}] = measure<T, 512, 6>(warpCount);
-    results[{warpCount, 7}] = measure<T, 512, 7>(warpCount);
-    results[{warpCount, 8}] = measure<T, 512, 8>(warpCount);
+  for (int warpCount = 1; warpCount <= maxWarpCount; warpCount *= 2) {
+    results[{warpCount, 1}] = measure<T, 128, 1>(warpCount);
+    results[{warpCount, 2}] = measure<T, 128, 2>(warpCount);
+    results[{warpCount, 3}] = measure<T, 128, 3>(warpCount);
+    results[{warpCount, 4}] = measure<T, 128, 4>(warpCount);
+    results[{warpCount, 5}] = measure<T, 128, 5>(warpCount);
+    results[{warpCount, 6}] = measure<T, 128, 6>(warpCount);
+    results[{warpCount, 7}] = measure<T, 128, 7>(warpCount);
+    results[{warpCount, 8}] = measure<T, 128, 8>(warpCount);
     cout << "\n";
   }
 
-  for (int warpCount = 1; warpCount <= maxWarpCount; warpCount++) {
+  for (int warpCount = 1; warpCount <= maxWarpCount; warpCount *= 2) {
     for (int streams = 1; streams <= 8; streams++) {
-      cout << setw(5) << setprecision(2) << results[{warpCount, streams}]
+      cout << setw(6) << setprecision(2) << results[{warpCount, streams}]
            << " ";
     }
     cout << "\n";
