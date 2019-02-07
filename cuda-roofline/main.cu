0abe84bdde509498b3d2dffaa4db58657f282f0e
Dominik Ernst <dominik.ernst@fau.de>
2019-02-07 14:59:16 +0100

    cuda-roofline: add nvml clock, power and temperature measurement

--- cuda-roofline/main.cu
+++ cuda-roofline/main.cu
@@ -1,17 +1,16 @@
-#include <cuComplex.h>
-#include <cuda_runtime.h>
-#include <omp.h>
-#include <sys/time.h>
-#include <iomanip>
-#include <iostream>
 #include "../dtime.hpp"
 #include "../gpu-error.h"
+#include <cuComplex.h>
+#include <cuda_runtime.h>
+#include <iomanip>
+#include <iostream>
+#include <nvml.h>
+#include <omp.h>
+#include <sys/time.h>
 
 using namespace std;
 
-
-template <typename T>
-__global__ void initKernel(T* data, size_t data_len) {
+template <typename T> __global__ void initKernel(T *data, size_t data_len) {
   int tidx = blockIdx.x * blockDim.x + threadIdx.x;
   for (int idx = tidx; idx < data_len; idx += gridDim.x * blockDim.x) {
     data[idx] = idx;
@@ -19,9 +18,9 @@ __global__ void initKernel(T* data, size_t data_len) {
 }
 
 template <typename T, int N, int M, int BLOCKSIZE>
-__global__ void testfun(T* dA, T* dB, T* dC) {
-  T* sA = dA + threadIdx.x + blockIdx.x * BLOCKSIZE * M;
-  T* sB = dB + threadIdx.x + blockIdx.x * BLOCKSIZE * M;
+__global__ void testfun(T *dA, T *dB, T *dC) {
+  T *sA = dA + threadIdx.x + blockIdx.x * BLOCKSIZE * M;
+  T *sB = dB + threadIdx.x + blockIdx.x * BLOCKSIZE * M;
 
   T sum = 0;
 
@@ -35,10 +34,13 @@ __global__ void testfun(T* dA, T* dB, T* dC) {
     }
     sum += v;
   }
-  if (threadIdx.x == 0) dC[blockIdx.x] = sum;
+  if (threadIdx.x == 0)
+    dC[blockIdx.x] = sum;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
+  nvmlInit();
+
   typedef double dtype;
   const int M = 1000;
   // PARN is a constant from the Makefile, set via -DPARN=X
@@ -58,15 +60,21 @@ int main(int argc, char** argv) {
     cudaGetDeviceProperties(&prop, deviceId);
     int numBlocks;
 
+    nvmlDevice_t device;
+    nvmlDeviceGetHandleByIndex(deviceId, &device);
+    unsigned int power = 0;
+    unsigned int clock = 0;
+    unsigned int temperature = 0;
+
     GPU_ERROR(cudaOccupancyMaxActiveBlocksPerMultiprocessor(
         &numBlocks, testfun<dtype, N, M, BLOCKSIZE>, BLOCKSIZE, 0));
     int blockCount = prop.multiProcessorCount * numBlocks;
 
     size_t data_len = (size_t)blockCount * BLOCKSIZE * M;
-    dtype* dA = NULL;
-    dtype* dB = NULL;
-    dtype* dC = NULL;
-    size_t iters = 200;
+    dtype *dA = NULL;
+    dtype *dB = NULL;
+    dtype *dC = NULL;
+    size_t iters = 10000;
 
     GPU_ERROR(cudaMalloc(&dA, data_len * sizeof(dtype)));
     GPU_ERROR(cudaMalloc(&dB, data_len * sizeof(dtype)));
@@ -76,6 +84,7 @@ int main(int argc, char** argv) {
     initKernel<<<blockCount, 256>>>(dB, data_len);
     initKernel<<<blockCount, 256>>>(dC, data_len);
     testfun<dtype, N, M, BLOCKSIZE><<<blockCount, BLOCKSIZE>>>(dA, dB, dC);
+
     cudaDeviceSynchronize();
 #pragma omp barrier
 
@@ -83,6 +92,9 @@ int main(int argc, char** argv) {
     for (size_t iter = 0; iter < iters; iter++) {
       testfun<dtype, N, M, BLOCKSIZE><<<blockCount, BLOCKSIZE>>>(dA, dB, dC);
     }
+    nvmlDeviceGetClockInfo(device, NVML_CLOCK_SM, &clock);
+    nvmlDeviceGetPowerUsage(device, &power);
+    nvmlDeviceGetTemperature(device, NVML_TEMPERATURE_GPU, &temperature);
     cudaDeviceSynchronize();
     double end = dtime();
     GPU_ERROR(cudaGetLastError());
@@ -90,13 +102,14 @@ int main(int argc, char** argv) {
 #pragma omp barrier
 #pragma omp critical
     {
-      cout << setprecision(3) << fixed << deviceId << " " << blockCount << " blocks   " << setw(3) << N
-           << " its      " << (2.0 + N * 2.0) / (2.0 * sizeof(dtype)) << " Fl/B      "
+      cout << setprecision(3) << fixed << deviceId << " " << blockCount
+           << " blocks   " << setw(3) << N << " its      "
+           << (2.0 + N * 2.0) / (2.0 * sizeof(dtype)) << " Fl/B      "
            << setprecision(0) << setw(5)
            << iters * 2 * data_len * sizeof(dtype) / (end - start) * 1.0e-9
            << " GB/s    " << setw(6)
            << iters * (2 + N * 2) * data_len / (end - start) * 1.0e-9
-           << " GF\n";
+           << " GF/s   " << clock << " Mhz   " << power / 1000 << " W   " << temperature << "°C\n";
     }
     GPU_ERROR(cudaFree(dA));
     GPU_ERROR(cudaFree(dB));
