6094fd7d775c7ab305f8f76cefe5a035e3d3a6cf
Dominik Ernst <dominik.ernst@fau.de>
2021-05-13 19:37:47 +0200

    cuda-stream: adapt buffer sizes, measure A100

--- cuda-stream/main.cu
+++ cuda-stream/main.cu
@@ -4,10 +4,11 @@
 #include "../metrics.cuh"
 #include <iomanip>
 #include <iostream>
+#include <nvml.h>
 
 using namespace std;
 
-const int max_buffer_size = 32 * 1024 * 1024;
+const int max_buffer_size = 512 * 1024 * 1024 + 2;
 double *dA, *dB, *dC, *dD;
 
 using kernel_ptr_type = void (*)(double *A, const double *__restrict__ B,
@@ -29,20 +30,20 @@ __global__ void sum_kernel(T *A, const T *__restrict__ B,
                            const T *__restrict__ C, const T *__restrict__ D,
                            const size_t N) {
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
-  double sum = 0.0;
+   double sum = 0.0;
 
-  for (size_t i = tidx; i < N - unroll * blockDim.x * gridDim.x;
+  for (int64_t i = tidx; i < N;
        i += blockDim.x * gridDim.x * unroll) {
 #pragma unroll
-    for (size_t u = 0; u < unroll; u++) {
-      sum += B[i + blockDim.x * gridDim.x * u];
-    }
+      for (int u = 0; u < unroll; u++) {
+          sum += B[i + blockDim.x * gridDim.x * u];
+      }
   }
 
-  for (size_t i = tidx + N - unroll * blockDim.x * gridDim.x; i < N;
-       i += blockDim.x * gridDim.x) {
-    sum += B[i];
-  }
+  // for (int64_t i = tidx + N - unroll * blockDim.x * gridDim.x; i < N && i >= 0;
+  //     i += blockDim.x * gridDim.x) {
+  //  sum += B[i];
+  // }
 
   if (tidx == 123123) {
     A[tidx] = sum;
@@ -78,8 +79,7 @@ __global__ void tdot_kernel(T *A, const T *__restrict__ B,
     A[tidx] = sum;
   }
 }
-
-template <typename T>
+ template <typename T>
 __global__ void scale_kernel(T *A, const T *__restrict__ B,
                              const T *__restrict__ C, const T *__restrict__ D,
                              const size_t N) {
@@ -87,6 +87,15 @@ __global__ void scale_kernel(T *A, const T *__restrict__ B,
   for (size_t i = tidx; i < N; i += blockDim.x * gridDim.x) {
     A[i] = 0.2 * B[i];
   }
+}
+
+ template <typename T>
+__global__ void scale_kernel_no_GSL(T *A, const T *__restrict__ B,
+                             const T *__restrict__ C, const T *__restrict__ D,
+                             const size_t N) {
+  int tidx = threadIdx.x + blockIdx.x * blockDim.x;
+  if( tidx >= N ) return;
+  A[tidx] = 0.2 * B[tidx];
 }
 
 template <typename T>
@@ -113,13 +122,20 @@ void measureFunc(kernel_ptr_type func, int stream_count, int block_count,
                  int block_size) {
 
   MeasurementSeries time;
+  MeasurementSeries power;
 
-  size_t buffer_size =
-    min((size_t) max_buffer_size,  ((size_t) block_size * block_count * max_buffer_size / 1024 ));
+  size_t buffer_size = min(8192, max_buffer_size / (block_count * block_size * 16)) * block_count * block_size * 16;
+
+
 
   func<<<block_count, block_size>>>(dA, dB, dC, dD, buffer_size);
 
-  for (int iter = 0; iter < 10; iter++) {
+  nvmlDevice_t device;
+  int deviceId;
+  cudaGetDevice(&deviceId);
+  nvmlDeviceGetHandleByIndex(deviceId, &device);
+
+  for (int iter = 0; iter < 9; iter++) {
     GPU_ERROR(cudaDeviceSynchronize());
     double t1 = dtime();
     GPU_ERROR(cudaDeviceSynchronize());
@@ -130,12 +146,22 @@ void measureFunc(kernel_ptr_type func, int stream_count, int block_count,
     time.add((t2 - t1) / 2);
   }
 
-  cout << fixed << setprecision(2)
+  // for (int iter = 0; iter < 100; iter++) {
+  //   func<<<block_count, block_size>>>(dA, dB, dC, dD, buffer_size);
+  //   func<<<block_count, block_size>>>(dA, dB, dC, dD, buffer_size);
+  //   unsigned int p;
+  //   nvmlDeviceGetPowerUsage(device, &p);
+  //   power.add(p);
+  //   GPU_ERROR(cudaDeviceSynchronize());
+  // }
+
+  cout << fixed << setprecision(0)
        << setw(6)
        //<< time.value() * 1000 << " "
        //<< setw(5) << time.spread() * 100
-       << "   " << setw(5)
-       << stream_count * buffer_size * sizeof(double) / time.value() * 1e-9;
+       //<< "   " << setw(5) << power.median() / 1000
+       << " " << setw(5)
+       << stream_count * buffer_size * sizeof(double) / time.median() * 1e-9;
   cout.flush();
 }
 
@@ -153,27 +179,28 @@ void measureKernels(vector<pair<kernel_ptr_type, int>> kernels, int block_count,
 }
 
 int main(int argc, char **argv) {
-
+  nvmlInit();
   GPU_ERROR(cudaMalloc(&dA, max_buffer_size * sizeof(double)));
   GPU_ERROR(cudaMalloc(&dB, max_buffer_size * sizeof(double)));
   GPU_ERROR(cudaMalloc(&dC, max_buffer_size * sizeof(double)));
   GPU_ERROR(cudaMalloc(&dD, max_buffer_size * sizeof(double)));
 
-  init_kernel<<<256, 400>>>(dA, dA, dA, dA, max_buffer_size);
+
   init_kernel<<<256, 400>>>(dB, dB, dB, dB, max_buffer_size);
+    init_kernel<<<256, 400>>>(dA, dA, dA, dA, max_buffer_size);
   init_kernel<<<256, 400>>>(dC, dC, dC, dC, max_buffer_size);
   init_kernel<<<256, 400>>>(dD, dD, dD, dD, max_buffer_size);
   GPU_ERROR(cudaDeviceSynchronize());
 
   vector<pair<kernel_ptr_type, int>> kernels = {
-      {init_kernel<double>, 1},     {sum_kernel<double, 1>, 1},
-      {sum_kernel<double, 2>, 1},   {sum_kernel<double, 4>, 1},
-      {sum_kernel<double, 8>, 1},   {sum_kernel<double, 16>, 1},
+      {init_kernel<double>, 1},      {sum_kernel<double, 1>, 1},
+       {sum_kernel<double, 2>, 1},   {sum_kernel<double, 4>, 1},
+       {sum_kernel<double, 8>, 1},   {sum_kernel<double, 16>, 1},
       {dot_kernel<double>, 2},      {tdot_kernel<double>, 3},
-      {scale_kernel<double>, 2},    {triad_kernel<double>, 3},
-      {sch_triad_kernel<double>, 4}};
+      {scale_kernel<double>, 2},
+      {triad_kernel<double>, 3}, {sch_triad_kernel<double>, 4}};
 
-  const int block_size = 128;
+  const int block_size = 256;
   cudaDeviceProp prop;
   int deviceId;
   GPU_ERROR(cudaGetDevice(&deviceId));
@@ -191,20 +218,21 @@ int main(int argc, char **argv) {
           "     sum4       sum8      sum16        dot       tdot      scale "
           "     triad  sch_triad\n";
 
-  for (int block_size = 32; block_size < 128; block_size *= 2) {
+   for (int block_size = 32; block_size <= 512; block_size *= 2) {
     measureKernels(kernels, 1, block_size, max_blocks);
   }
 
-  for (int i = 1; i < smCount; i *= 2) {
+  for (int i = 2; i < smCount; i*=2) {
     measureKernels(kernels, i, block_size, max_blocks);
   }
 
-  for (int i = 1 * smCount; i <= smCount * maxActiveBlocks; i += smCount) {
+  for (int i = smCount; i <= smCount * maxActiveBlocks; i += smCount) {
     measureKernels(kernels, i, block_size, max_blocks);
   }
 
-  GPU_ERROR(cudaFree(dA));
-  GPU_ERROR(cudaFree(dB));
+
+   GPU_ERROR(cudaFree(dA));
+   GPU_ERROR(cudaFree(dB));
   GPU_ERROR(cudaFree(dC));
-  GPU_ERROR(cudaFree(dD));
+   GPU_ERROR(cudaFree(dD));
 }
