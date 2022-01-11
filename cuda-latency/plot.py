b7b1ec2fb4fb32890a85f35a182707de48cb7a93
Dominik Ernst <dominik.ernst@gmx.de>
2022-01-11 15:57:12 +0100

    add cuda-latency plot and plot script

---
+++ cuda-latency/plot.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import os
+import csv
+import matplotlib.pyplot as plt
+import matplotlib
+
+fig,ax = plt.subplots(figsize=(9,4))
+for filename in os.listdir("."):
+    if not filename.endswith(".txt"):
+        continue
+    with open(filename, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=" ", skipinitialspace=True)
+        sizes = []
+        bw = []
+        for row in csvreader:
+            if row[0] == "data":
+                continue
+            sizes.append(float(row[1]))
+            bw.append(float(row[3]))
+
+        print(sizes)
+        print(bw)
+        ax.plot(sizes, bw, "-o", label=filename, linewidth=0.4, markeredgewidth=0, alpha=0.66, markersize=3)
+
+ax.set_xlabel("chain data volume, kB")
+ax.set_ylabel("latency, cycles")
+ax.set_xscale("log", base=2)
+
+ax.set_xticks(
+    [64, 128, 192, 256, 1024, 6*1024, 20 * 1024, 40 * 1024]
+)
+
+ax.axvline(196)
+ax.axvline(20*1024)
+ax.axvline(40*1024)
+
+formatter = matplotlib.ticker.FuncFormatter(lambda x, pos: "{0:g}".format(x))
+ax.get_xaxis().set_major_formatter(formatter)
+ax.get_yaxis().set_major_formatter(formatter)
+
+ax.legend()
+ax.set_ylim([0, ax.get_ylim()[1]])
+fig.savefig("cache_plot.svg")
