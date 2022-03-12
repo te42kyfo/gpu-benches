3f57da70ffeb897352e9c88cbe780bf5de996053
Dominik Ernst <dominik.ernst@gmx.de>
2022-03-12 15:10:13 +0100

    update cuda-stream and readme

--- cuda-stream/main.cu
+++ cuda-stream/main.cu
@@ -1,15 +1,16 @@
 #include "../MeasurementSeries.hpp"
 #include "../dtime.hpp"
 #include "../gpu-error.h"
-#include "../metrics.cuh"
 #include <iomanip>
 #include <iostream>
 #include <nvml.h>
 
 using namespace std;
 
-const int max_buffer_size = 512 * 1024 * 1024 + 2;
+const int64_t max_buffer_size = 128l * 1024 * 1024 + 2;
 double *dA, *dB, *dC, *dD;
+
+const int spoilerSize = 768;
 
 using kernel_ptr_type = void (*)(double *A, const double *__restrict__ B,
                                  const double *__restrict__ C,
@@ -19,141 +20,145 @@ template <typename T>
 __global__ void init_kernel(T *A, const T *__restrict__ B,
                             const T *__restrict__ C, const T *__restrict__ D,
                             const size_t N) {
+  __shared__ double spoiler[spoilerSize];
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
-  for (size_t i = tidx; i < N; i += blockDim.x * gridDim.x) {
-    A[i] = 0.1;
-  }
-}
+  if (tidx >= N)
+    return;
 
-template <typename T, int unroll>
-__global__ void sum_kernel(T *A, const T *__restrict__ B,
-                           const T *__restrict__ C, const T *__restrict__ D,
-                           const size_t N) {
-  int tidx = threadIdx.x + blockIdx.x * blockDim.x;
-   double sum = 0.0;
+  if (threadIdx.x > 1243)
+    spoiler[threadIdx.x] = B[threadIdx.x];
 
-  for (int64_t i = tidx; i < N;
-       i += blockDim.x * gridDim.x * unroll) {
-#pragma unroll
-      for (int u = 0; u < unroll; u++) {
-          sum += B[i + blockDim.x * gridDim.x * u];
-      }
-  }
+  A[tidx] = 0.23;
 
-  // for (int64_t i = tidx + N - unroll * blockDim.x * gridDim.x; i < N && i >= 0;
-  //     i += blockDim.x * gridDim.x) {
-  //  sum += B[i];
-  // }
-
-  if (tidx == 123123) {
-    A[tidx] = sum;
-  }
+  if (threadIdx.x > 1243)
+    A[tidx] = spoiler[tidx];
 }
 
 template <typename T>
-__global__ void dot_kernel(T *A, const T *__restrict__ B,
-                           const T *__restrict__ C, const T *__restrict__ D,
-                           const size_t N) {
-  int tidx = threadIdx.x + blockIdx.x * blockDim.x;
-  double sum = 0.0;
-  for (size_t i = tidx; i < N; i += blockDim.x * gridDim.x) {
-    sum += B[i] * C[i];
-  }
-
-  if (tidx == 123123) {
-    A[tidx] = sum;
-  }
-}
-
-template <typename T>
-__global__ void tdot_kernel(T *A, const T *__restrict__ B,
+__global__ void read_kernel(T *A, const T *__restrict__ B,
                             const T *__restrict__ C, const T *__restrict__ D,
                             const size_t N) {
+  __shared__ double spoiler[spoilerSize];
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
-  double sum = 0.0;
-  for (size_t i = tidx; i < N; i += blockDim.x * gridDim.x) {
-    sum += B[i] * C[i] * D[i];
-  }
+  if (tidx >= N)
+    return;
 
-  if (tidx == 123123) {
-    A[tidx] = sum;
-  }
+  if (threadIdx.x > 1243)
+    spoiler[threadIdx.x] = B[threadIdx.x];
+
+  double temp = B[tidx];
+
+  if (temp == 123.33)
+    A[tidx] = temp;
+
+  if (threadIdx.x > 1243)
+    A[tidx] = spoiler[tidx];
 }
- template <typename T>
+
+template <typename T>
 __global__ void scale_kernel(T *A, const T *__restrict__ B,
                              const T *__restrict__ C, const T *__restrict__ D,
                              const size_t N) {
+  __shared__ double spoiler[spoilerSize];
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
-  for (size_t i = tidx; i < N; i += blockDim.x * gridDim.x) {
-    A[i] = 0.2 * B[i];
-  }
-}
+  if (tidx >= N)
+    return;
 
- template <typename T>
-__global__ void scale_kernel_no_GSL(T *A, const T *__restrict__ B,
-                             const T *__restrict__ C, const T *__restrict__ D,
-                             const size_t N) {
-  int tidx = threadIdx.x + blockIdx.x * blockDim.x;
-  if( tidx >= N ) return;
-  A[tidx] = 0.2 * B[tidx];
+  if (threadIdx.x > 1243)
+    spoiler[threadIdx.x] = B[threadIdx.x];
+
+  A[tidx] = B[tidx] * 1.2;
+
+  if (threadIdx.x > 1243)
+    A[tidx] = spoiler[tidx];
 }
 
 template <typename T>
 __global__ void triad_kernel(T *A, const T *__restrict__ B,
                              const T *__restrict__ C, const T *__restrict__ D,
                              const size_t N) {
+  __shared__ double spoiler[spoilerSize];
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
-  for (size_t i = tidx; i < N; i += blockDim.x * gridDim.x) {
-    A[i] = B[i] + 0.2 * C[i];
-  }
+  if (tidx >= N)
+    return;
+
+  if (threadIdx.x > 1243)
+    spoiler[threadIdx.x] = B[threadIdx.x];
+
+  A[tidx] = B[tidx] * 1.2 + C[tidx];
+
+  if (threadIdx.x > 1243)
+    A[tidx] = spoiler[tidx];
 }
 
 template <typename T>
-__global__ void sch_triad_kernel(T *A, const T *__restrict__ B,
-                                 const T *__restrict__ C,
-                                 const T *__restrict__ D, const size_t N) {
+__global__ void stencil1d3pt_kernel(T *A, const T *__restrict__ B,
+                                    const T *__restrict__ C,
+                                    const T *__restrict__ D, const size_t N) {
+  __shared__ double spoiler[spoilerSize];
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
-  for (size_t i = tidx; i < N; i += blockDim.x * gridDim.x) {
-    A[i] = B[i] + C[i] * D[i];
-  }
+  if (tidx >= N - 1 || tidx == 0)
+    return;
+
+  if (threadIdx.x > 1243)
+    spoiler[threadIdx.x] = B[threadIdx.x];
+
+  A[tidx] = 0.5 * B[tidx - 1] - 1.0 * B[tidx] + 0.5 * B[tidx + 1];
+
+  if (threadIdx.x > 1243)
+    A[tidx] = spoiler[tidx];
 }
+template <typename T>
+__global__ void stencil1d5pt_kernel(T *A, const T *__restrict__ B,
+                                    const T *__restrict__ C,
+                                    const T *__restrict__ D, const size_t N) {
+  __shared__ double spoiler[spoilerSize];
+  int tidx = threadIdx.x + blockIdx.x * blockDim.x;
+  if (tidx >= N - 2 || tidx < 2)
+    return;
 
-void measureFunc(kernel_ptr_type func, int stream_count, int block_count,
-                 int block_size) {
+  if (threadIdx.x > 1243)
+    spoiler[threadIdx.x] = B[threadIdx.x];
 
+  A[tidx] = 0.25 * B[tidx - 2] + 0.25 * B[tidx - 1] - 1.0 * B[tidx] +
+            0.5 * B[tidx + 1] + 0.5 * B[tidx + 2];
+
+  if (threadIdx.x > 1243)
+    A[tidx] = spoiler[tidx];
+}
+void measureFunc(kernel_ptr_type func, int streamCount, int blockSize,
+                 int blocksPerSM) {
+  if (blocksPerSM == 1) {
+    GPU_ERROR(cudaFuncSetAttribute(
+        func, cudaFuncAttributePreferredSharedMemoryCarveout, 4));
+  } else {
+    GPU_ERROR(cudaFuncSetAttribute(
+        func, cudaFuncAttributePreferredSharedMemoryCarveout, 9));
+  }
+
+  int maxActiveBlocks = 0;
+  GPU_ERROR(cudaOccupancyMaxActiveBlocksPerMultiprocessor(&maxActiveBlocks,
+                                                          func, blockSize, 0));
+  if (maxActiveBlocks != blocksPerSM)
+    cout << "! " << maxActiveBlocks << " blocks per SM ";
   MeasurementSeries time;
   MeasurementSeries power;
 
-  size_t buffer_size = min(8192, max_buffer_size / (block_count * block_size * 16)) * block_count * block_size * 16;
-
-
-
-  func<<<block_count, block_size>>>(dA, dB, dC, dD, buffer_size);
-
-  nvmlDevice_t device;
-  int deviceId;
-  cudaGetDevice(&deviceId);
-  nvmlDeviceGetHandleByIndex(deviceId, &device);
+  func<<<max_buffer_size / blockSize + 1, blockSize>>>(dA, dB, dC, dD,
+                                                       max_buffer_size);
 
   for (int iter = 0; iter < 9; iter++) {
     GPU_ERROR(cudaDeviceSynchronize());
     double t1 = dtime();
     GPU_ERROR(cudaDeviceSynchronize());
-    func<<<block_count, block_size>>>(dA, dB, dC, dD, buffer_size);
-    func<<<block_count, block_size>>>(dA, dB, dC, dD, buffer_size);
+    func<<<max_buffer_size / blockSize + 1, blockSize>>>(dA, dB, dC, dD,
+                                                         max_buffer_size);
+    func<<<max_buffer_size / blockSize + 1, blockSize>>>(dA, dB, dC, dD,
+                                                         max_buffer_size);
     GPU_ERROR(cudaDeviceSynchronize());
     double t2 = dtime();
     time.add((t2 - t1) / 2);
   }
-
-  // for (int iter = 0; iter < 100; iter++) {
-  //   func<<<block_count, block_size>>>(dA, dB, dC, dD, buffer_size);
-  //   func<<<block_count, block_size>>>(dA, dB, dC, dD, buffer_size);
-  //   unsigned int p;
-  //   nvmlDeviceGetPowerUsage(device, &p);
-  //   power.add(p);
-  //   GPU_ERROR(cudaDeviceSynchronize());
-  // }
 
   cout << fixed << setprecision(0)
        << setw(6)
@@ -161,18 +166,27 @@ void measureFunc(kernel_ptr_type func, int stream_count, int block_count,
        //<< setw(5) << time.spread() * 100
        //<< "   " << setw(5) << power.median() / 1000
        << " " << setw(5)
-       << stream_count * buffer_size * sizeof(double) / time.median() * 1e-9;
+       << streamCount * max_buffer_size * sizeof(double) / time.median() * 1e-9;
   cout.flush();
 }
 
-void measureKernels(vector<pair<kernel_ptr_type, int>> kernels, int block_count,
-                    int block_size, int max_blocks) {
-  cout << setw(9) << block_count << "   " << setw(10)
-       << block_size * block_count << "  " << setw(7) << setprecision(1)
-       << (double)block_count / max_blocks * 100.0 << "  |  GB/s: ";
+void measureKernels(vector<pair<kernel_ptr_type, int>> kernels, int blockSize,
+                    int blocksPerSM) {
+  cudaDeviceProp prop;
+  int deviceId;
+  GPU_ERROR(cudaGetDevice(&deviceId));
+  nvmlDevice_t device;
+  nvmlDeviceGetHandleByIndex(deviceId, &device);
+  GPU_ERROR(cudaGetDeviceProperties(&prop, deviceId));
+  std::string deviceName = prop.name;
+  int smCount = prop.multiProcessorCount;
+
+  cout << setw(9) << blockSize << "   " << setw(9) << smCount * blockSize
+       << "  " << setw(5) << setprecision(1)
+       << blockSize / 1024.0 * 50.0 * blocksPerSM << " %  |  GB/s: ";
 
   for (auto kernel : kernels) {
-    measureFunc(kernel.first, kernel.second, block_count, block_size);
+    measureFunc(kernel.first, kernel.second, blockSize, blocksPerSM);
   }
 
   cout << "\n";
@@ -185,54 +199,36 @@ int main(int argc, char **argv) {
   GPU_ERROR(cudaMalloc(&dC, max_buffer_size * sizeof(double)));
   GPU_ERROR(cudaMalloc(&dD, max_buffer_size * sizeof(double)));
 
-
-  init_kernel<<<256, 400>>>(dB, dB, dB, dB, max_buffer_size);
-    init_kernel<<<256, 400>>>(dA, dA, dA, dA, max_buffer_size);
-  init_kernel<<<256, 400>>>(dC, dC, dC, dC, max_buffer_size);
-  init_kernel<<<256, 400>>>(dD, dD, dD, dD, max_buffer_size);
+  init_kernel<<<max_buffer_size / 1024 + 1, 1024>>>(dA, dA, dA, dA,
+                                                    max_buffer_size);
+  init_kernel<<<max_buffer_size / 1024 + 1, 1024>>>(dB, dB, dB, dB,
+                                                    max_buffer_size);
+  init_kernel<<<max_buffer_size / 1024 + 1, 1024>>>(dC, dC, dC, dC,
+                                                    max_buffer_size);
+  init_kernel<<<max_buffer_size / 1024 + 1, 1024>>>(dD, dD, dD, dD,
+                                                    max_buffer_size);
   GPU_ERROR(cudaDeviceSynchronize());
 
-  vector<pair<kernel_ptr_type, int>> kernels = {
-      {init_kernel<double>, 1},      {sum_kernel<double, 1>, 1},
-       {sum_kernel<double, 2>, 1},   {sum_kernel<double, 4>, 1},
-       {sum_kernel<double, 8>, 1},   {sum_kernel<double, 16>, 1},
-      {dot_kernel<double>, 2},      {tdot_kernel<double>, 3},
-      {scale_kernel<double>, 2},
-      {triad_kernel<double>, 3}, {sch_triad_kernel<double>, 4}};
+  vector<pair<kernel_ptr_type, int>> kernels = {{init_kernel<double>, 1},
+                                                {read_kernel<double>, 1},
+                                                {scale_kernel<double>, 2},
+                                                {triad_kernel<double>, 3},
+                                                {stencil1d3pt_kernel<double>, 2},
+                                                {stencil1d5pt_kernel<double>, 2}};
 
-  const int block_size = 256;
-  cudaDeviceProp prop;
-  int deviceId;
-  GPU_ERROR(cudaGetDevice(&deviceId));
-  GPU_ERROR(cudaGetDeviceProperties(&prop, deviceId));
-  std::string deviceName = prop.name;
-  int smCount = prop.multiProcessorCount;
-  int maxActiveBlocks = 0;
-  GPU_ERROR(cudaOccupancyMaxActiveBlocksPerMultiprocessor(
-      &maxActiveBlocks, kernels[0].first, block_size, 0));
+  cout << "blockSize   threads       %occ  |                init"
+       << "       read       scale     triad       1pt        3pt\n";
 
-  int max_blocks = maxActiveBlocks * smCount;
+  // for (int blockSize = 32; blockSize <= 1024; blockSize += 32) {
+  //   measureKernels(kernels, blockSize, 1);
+  // }
 
-  cout << "    blocks     threads     %occ  |               init       sum1    "
-          "   sum2  "
-          "     sum4       sum8      sum16        dot       tdot      scale "
-          "     triad  sch_triad\n";
-
-   for (int block_size = 32; block_size <= 512; block_size *= 2) {
-    measureKernels(kernels, 1, block_size, max_blocks);
+  for (int blockSize = 32; blockSize <= 1024; blockSize += 32) {
+    measureKernels(kernels, blockSize, 2);
   }
 
-  for (int i = 2; i < smCount; i*=2) {
-    measureKernels(kernels, i, block_size, max_blocks);
-  }
-
-  for (int i = smCount; i <= smCount * maxActiveBlocks; i += smCount) {
-    measureKernels(kernels, i, block_size, max_blocks);
-  }
-
-
-   GPU_ERROR(cudaFree(dA));
-   GPU_ERROR(cudaFree(dB));
+  GPU_ERROR(cudaFree(dA));
+  GPU_ERROR(cudaFree(dB));
   GPU_ERROR(cudaFree(dC));
-   GPU_ERROR(cudaFree(dD));
+  GPU_ERROR(cudaFree(dD));
 }
--- cuda-stream/plot.py
+++ cuda-stream/plot.py
@@ -4,6 +4,11 @@ import os
 import csv
 import matplotlib.pyplot as plt
 
+
+fig,ax = plt.subplots(figsize=(9,4))
+
+color = 0
+
 for filename in os.listdir("."):
     if not filename.endswith(".txt"):
         continue
@@ -12,38 +17,41 @@ for filename in os.listdir("."):
         threads = []
         locs = []
         init = []
-        sum1 = []
-        sum4 = []
+        read = []
         scale = []
         triad = []
 
         for row in csvreader:
-            if row[0] == "blocks":
+            if row[0] == "blockSize":
                 continue
-            threads.append(row[2])
-            init.append(float(row[5]))
-            sum1.append(float(row[6]))
-            sum4.append(float(row[8]))
-            scale.append(float(row[13]))
-            triad.append(float(row[14]))
+            threads.append(int(row[1]))
+            init.append(float(row[6]))
+            read.append(float(row[7]))
+            scale.append(float(row[8]))
+            triad.append(float(row[9]))
             locs.append(float(row[2]))
 
-        locs = [15 + l / 6 if l > 15 else l for l in locs]
+        #locs = threads#[15 + l / 6 if l > 15 else l for l in locs]
         print(locs)
         print(threads)
-        fig,ax = plt.subplots(figsize=(9,4))
-        ax.plot(locs, init, "-x", label="init")
-        ax.plot(locs, sum1, "-x", label="sum1")
-        ax.plot(locs, sum4, "-x", label="sum4")
-        ax.plot(locs, scale, "-x", label="scale")
-        ax.plot(locs, triad, "-x", label="triad")
-        ax.set_xticks(locs)
-        ax.set_xticklabels(threads, rotation="vertical")
-        ax.set_xlabel("Occupancy, %")
-        ax.set_ylabel("GB/s")
+        #ax.plot(locs, init,  "-v", label=filename, color="C" + str(color))
+        ax.plot(threads, scale, "-o", label=filename[:-4].upper(), color="C" + str(color))
+        #ax.plot(threads, triad, "-<", label=filename, color="C" + str(color))
+        #ax.plot(threads, read, "-^", label=filename, color="C" + str(color))
 
+    color += 1
 
-        fig.tight_layout()
-        ax.legend()
-        ax.set_ylim([0, ax.get_ylim()[1]])
-        fig.savefig(filename + ".svg")
+ax.set_xticks(threads[::5])
+#ax.set_xticklabels(threads, rotation="vertical")
+ax.set_xlabel("threads")
+ax.set_ylabel("GB/s")
+
+#ax.axhline(1400, linestyle="--", color="C1")
+#ax.axhline(800, linestyle="--", color="C0")
+
+ax.grid()
+fig.tight_layout()
+ax.legend()
+ax.set_ylim([0, ax.get_ylim()[1]])
+plt.show()
+fig.savefig(filename + ".svg")
