acc5ba75af801102bbdd6105f0b91fbcff9f8aea
Dominik Ernst <dominik.ernst@fau.de>
2022-07-15 14:48:20 +0200

    cuda-cache: add plotting and change benchmark methodology

--- cuda-cache/main.cu
+++ cuda-cache/main.cu
@@ -2,12 +2,13 @@
 #include "../dtime.hpp"
 #include "../gpu-error.h"
 #include "../metrics.cuh"
+#include "../measure_metric/measureMetricPW.hpp"
 #include <iomanip>
 #include <iostream>
 
 using namespace std;
 
-double *dA, *dB, *dC;
+double *dA, *dB;
 
 __global__ void initKernel(double *A, size_t N) {
   size_t tidx = blockDim.x * blockIdx.x + threadIdx.x;
@@ -17,13 +18,13 @@ __global__ void initKernel(double *A, size_t N) {
 }
 
 template <int N, int iters, int BLOCKSIZE>
-__global__ void daxpyKernel(double *A, double *B, double *C) {
+__global__ void sumKernel(double * __restrict__ A, const double * __restrict__ B, int zero) {
   double localSum = 0;
 #pragma unroll(1)
   for (int iter = 0; iter < iters; iter++) {
     for (int i = 0; i < N; i += BLOCKSIZE) {
-      int idx = i + threadIdx.x;
-      localSum += B[idx] * C[idx];
+      int idx = i + threadIdx.x + iter*zero;
+      localSum += B[idx];
     }
     localSum *= 1.3;
     if (threadIdx.x > 1233)
@@ -34,14 +35,14 @@ __global__ void daxpyKernel(double *A, double *B, double *C) {
 }
 
 template <int N, int iters, int blockSize> double callKernel(int blockCount) {
-  daxpyKernel<N, iters, blockSize><<<blockCount, blockSize>>>(dA, dB, dC);
+  sumKernel<N, iters, blockSize><<<blockCount, blockSize>>>(dA, dB, 0);
   return 0.0;
 }
 
 template <int N> void measure() {
-  const int iters = 100000000 / N + 1;
+  const int iters = 100000000 / N + 2;
 
-  const int blockSize = 256;
+  const int blockSize = 1024;
 
   cudaDeviceProp prop;
   int deviceId;
@@ -51,79 +52,61 @@ template <int N> void measure() {
   int smCount = prop.multiProcessorCount;
   int maxActiveBlocks = 0;
   GPU_ERROR(cudaOccupancyMaxActiveBlocksPerMultiprocessor(
-      &maxActiveBlocks, daxpyKernel<N, iters, blockSize>, blockSize, 0));
+      &maxActiveBlocks, sumKernel<N, iters, blockSize>, blockSize, 0));
 
   int blockCount = smCount * 1; // maxActiveBlocks;
 
   MeasurementSeries time;
+  MeasurementSeries dram_read;
+  MeasurementSeries dram_write;
+  MeasurementSeries L2_read;
+  MeasurementSeries L2_write;
 
   GPU_ERROR(cudaDeviceSynchronize());
   for (int i = 0; i < 15; i++) {
-    GPU_ERROR(cudaMalloc(&dA, (N + i * 128) * sizeof(double)));
-    initKernel<<<52, 256>>>(dA, N + i * 128);
-    GPU_ERROR(cudaMalloc(&dB, (N + i * 128) * sizeof(double)));
-    initKernel<<<52, 256>>>(dB, N + i * 128);
-    GPU_ERROR(cudaMalloc(&dC, (N + i * 128) * sizeof(double)));
-    initKernel<<<52, 256>>>(dC, N + i * 128);
+    const size_t bufferCount = N + i * 128;
+    GPU_ERROR(cudaMalloc(&dA, bufferCount * sizeof(double)));
+    initKernel<<<52, 256>>>(dA, bufferCount);
+    GPU_ERROR(cudaMalloc(&dB, bufferCount* sizeof(double)));
+    initKernel<<<52, 256>>>(dB, bufferCount);
     GPU_ERROR(cudaDeviceSynchronize());
 
     double t1 = dtime();
     callKernel<N, iters, blockSize>(blockCount);
+    callKernel<N, iters, blockSize>(blockCount);
     GPU_ERROR(cudaDeviceSynchronize());
     double t2 = dtime();
-    time.add(t2 - t1);
+    time.add((t2 - t1)/2);
+
+    measureBandwidthStart();
+    callKernel<N, iters, blockSize>(blockCount);
+    auto metrics = measureMetricStop();
+
+    dram_read.add(metrics[0]);
+    dram_write.add(metrics[1]);
+    L2_read.add(metrics[2]*32);
+    L2_write.add(metrics[3]*32);
 
     cudaFree(dA);
     cudaFree(dB);
-    cudaFree(dC);
   }
-  GPU_ERROR(cudaMalloc(&dA, N * sizeof(double)));
-  initKernel<<<52, 256>>>(dA, N);
-  GPU_ERROR(cudaMalloc(&dB, N * sizeof(double)));
-  initKernel<<<52, 256>>>(dB, N);
-  GPU_ERROR(cudaMalloc(&dC, N * sizeof(double)));
-  initKernel<<<52, 256>>>(dC, N);
-
-  GPU_ERROR(cudaDeviceSynchronize());
-  std::function<double()> measureKernelFunction =
-      std::bind(callKernel<N, iters , blockSize>, blockCount);
-
-  double dramReadBW =
-      measureMetric(measureKernelFunction, "dram_read_throughput") / 1e9;
-  double dramWriteBW =
-      measureMetric(measureKernelFunction, "dram_write_throughput") / 1e9;
-
-  double L2ReadBW =
-      measureMetric(measureKernelFunction, "l2_read_throughput") / 1e9;
-
-  double L2WriteBW =
-      measureMetric(measureKernelFunction, "l2_write_throughput") / 1e9;
-
-  double texReadBW =
-      measureMetric(measureKernelFunction, "tex_cache_throughput") / 1e9;
-
-  cudaFree(dA);
-  cudaFree(dB);
-  cudaFree(dC);
-
-  double blockDV = N * sizeof(double) * 2;
+  double blockDV = N * sizeof(double);
 
   double bw = blockDV * blockCount * iters / time.value() / 1.0e9;
   cout << fixed << setprecision(0) << setw(10) << blockDV / 1024 << " kB" //
        << setprecision(0) << setw(10) << time.value() * 1000.0 << "ms"    //
        << setprecision(1) << setw(10) << time.spread() * 100 << "%"       //
-       << setw(10) << bw << " GB/s"                                        //
-       << setw(10) << dramReadBW << " GB/s"                                //
-       << setw(10) << dramWriteBW << " GB/s"                               //
-       << setw(10) << L2ReadBW << " GB/s"                                  //
-       << setw(10) << L2WriteBW << " GB/s"                                 //
-       << setw(10) << texReadBW << " GB/s\n";
+       << setw(10) << bw << " GB/s"                                    //
+       << setprecision(0) << setw(10) << dram_read.value() / time.value() / 1.0e9 << " GB/s "    //
+       << setprecision(0) << setw(10) << dram_write.value() / time.value() / 1.0e9 << " GB/s "    //
+       << setprecision(0) << setw(10) << L2_read.value() / time.value() / 1.0e9 << " GB/s "    //
+       << setprecision(0) << setw(10) << L2_write.value() / time.value() / 1.0e9 << " GB/s " << endl;   //
 }
 
 size_t constexpr expSeries(size_t N) {
     size_t val = 32*512;
     for( size_t i  = 0; i < N; i++) {
-        val *= 1.15;
+        val *= 1.16;
     }
     return (val / 512) * 512;
 }
@@ -134,14 +117,9 @@ int main(int argc, char **argv) {
   cout << setw(13) << "data set" //
        << setw(12) << "exec time" //
        << setw(11) << "spread"    //
-       << setw(15) << "Eff. bw"   //
-       << setw(15) << "DRAM read"  //
-       << setw(15) << "DRAM write" //
-       << setw(15) << "L2 Read"    //
-       << setw(15) << "L2 Write"   //
-       << setw(15) << "Tex Read\n";
+       << setw(15) << "Eff. bw\n";   //
 
-  measure<1*512>();
+
   measure<2*512>();
   measure<3*512>();
   measure<4*512>();
@@ -192,4 +170,34 @@ int main(int argc, char **argv) {
   measure<expSeries(17)>();
   measure<expSeries(18)>();
   measure<expSeries(19)>();
+  measure<expSeries(20)>();
+  measure<expSeries(21)>();
+  measure<expSeries(22)>();
+  measure<expSeries(23)>();
+  measure<expSeries(24)>();
+  measure<expSeries(25)>();
+  measure<expSeries(26)>();
+  measure<expSeries(27)>();
+  measure<expSeries(28)>();
+  measure<expSeries(29)>();
+  measure<expSeries(30)>();
+  measure<expSeries(31)>();
+  measure<expSeries(32)>();
+  measure<expSeries(33)>();
+  measure<expSeries(34)>();
+  measure<expSeries(35)>();
+  measure<expSeries(36)>();
+  measure<expSeries(37)>();
+  measure<expSeries(38)>();
+  measure<expSeries(39)>();
+  measure<expSeries(40)>();
+  measure<expSeries(41)>();
+  measure<expSeries(42)>();
+  measure<expSeries(43)>();
+  measure<expSeries(44)>();
+  measure<expSeries(45)>();
+  measure<expSeries(46)>();
+  measure<expSeries(47)>();
+  measure<expSeries(48)>();
+  measure<expSeries(49)>();
 }
--- cuda-cache/plot.py
+++ cuda-cache/plot.py
@@ -3,28 +3,54 @@
 import os
 import csv
 import matplotlib.pyplot as plt
+import matplotlib
 
-fig,ax = plt.subplots(figsize=(9,4))
-for filename in os.listdir("."):
+fig,ax = plt.subplots(figsize=(6,3.5))
+
+for filename in sorted( os.listdir(".") ):
     if not filename.endswith(".txt"):
         continue
+
     with open(filename, newline='') as csvfile:
+        if filename[0] == 'a':
+            color = "C1"
+        else:
+            color = "C0"
+        if filename[5] == 's':
+            style = "-o"
+        else:
+            style = "-^"
+
         csvreader = csv.reader(csvfile, delimiter=" ", skipinitialspace=True)
         sizes = []
         bw = []
         for row in csvreader:
             if row[0] == "data":
                 continue
-            sizes.append(float(row[0]))
-            bw.append(float(row[4]))
+            sizes.append(float(row[2]))
+            bw.append(float(row[6]))
 
         print(sizes)
         print(bw)
-        ax.plot(sizes, bw, "-x", label=filename)
+        ax.plot(sizes, bw, style, color=color, label=filename[:-4].upper())
 
-ax.set_xlabel("dataset per thread block, kB")
+ax.set_xlabel("data volume, MB")
 ax.set_ylabel("GB/s")
-ax.set_xscale('log')
+
+ax.set_xscale("log")
+
+
+#ax.axvline(20*1024, color="C1")
+#ax.axvline(40*1024, color="C1")
+#ax.axvline(6*1024, color="C0")
+
+ax.grid()
 ax.legend()
-ax.set_ylim([0, ax.get_ylim()[1]])
-fig.savefig("cache_plot.svg")
+ax.set_xticks([6*1024, 20*1024, 40*1024])
+ax.set_xticklabels([6, 20, 40])
+ax.set_yticks([800, 2500, 1400, 5000])
+ax.set_ylim([0, ax.get_ylim()[1]*1.1])
+
+fig.tight_layout()
+plt.show()
+fig.savefig("cuda-cache.pdf")
