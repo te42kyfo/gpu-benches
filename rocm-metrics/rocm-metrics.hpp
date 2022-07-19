2c882f46e2d169fbabda029cbbd3fc2c2471bae2
Dominik Ernst <dominik.ernst@fau.de>
2022-07-19 14:10:14 +0200

    rocm-metrics: second attempt

--- rocm-metrics/rocm-metrics.hpp
+++ rocm-metrics/rocm-metrics.hpp
@@ -23,155 +23,311 @@ THE SOFTWARE.
 
 #include <vector>
 #include <iostream>
-#include <atomic>
 #include "rocprofiler.h"
 
+#include <hsa.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+#include <vector>
+#include <atomic>
 
-// Check returned HSA API status
-void check_status(hsa_status_t status) {
-  if (status != HSA_STATUS_SUCCESS) {
-    const char* error_string = NULL;
-    rocprofiler_error_string(&error_string);
-    std::cout << "rocprof ERROR: %s\n";
-    abort();
-  }
+#include <stdatomic.h>
+
+typedef struct {
+#define MAX_DEVICE_COUNT 16
+    rocprofiler_pool_t *pools[MAX_DEVICE_COUNT];
+} callback_arg_t;
+
+typedef struct {
+    rocprofiler_feature_t *features;
+    unsigned feature_count;
+} handler_arg_t;
+
+typedef struct {
+    bool valid;
+    hsa_agent_t agent;
+    rocprofiler_group_t group;
+    rocprofiler_callback_data_t data;
+} context_entry_t;
+
+typedef struct {
+    hsa_agent_t agent;
+} agent_info_t;
+
+/* Data structures handled by init and shutdown */
+handler_arg_t *handler_arg;
+callback_arg_t callback_arg[1];
+agent_info_t agent_info_arr[16];
+unsigned agent_info_arr_len;
+rocprofiler_feature_t *features;
+pthread_mutex_t feature_lock = PTHREAD_MUTEX_INITIALIZER;
+
+static unsigned _get_gpu_id(hsa_agent_t agent)
+{
+    unsigned gpu_id;
+    for (gpu_id = 0; gpu_id < agent_info_arr_len; ++gpu_id) {
+        if (memcmp(&agent_info_arr[gpu_id].agent, &agent, sizeof(hsa_agent_t)) == 0) {
+            return gpu_id;
+        }
+    }
+    return -1;
 }
 
-pthread_mutex_t mutex = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
-
-// Context stored entry type
-struct context_entry_t {
-  bool valid;
-  hsa_agent_t agent;
-  rocprofiler_group_t group;
-  rocprofiler_callback_data_t data;
-};
-
-// Dump stored context entry
-void dump_context_entry(context_entry_t* entry) {
-  volatile std::atomic<bool>* valid = reinterpret_cast<std::atomic<bool>*>(&entry->valid);
-  while (valid->load() == false) sched_yield();
-
-  const std::string kernel_name = entry->data.kernel_name;
-  const rocprofiler_dispatch_record_t* record = entry->data.record;
-
-
-  std::cout << "kernel-object name" <<  kernel_name.c_str() << "\n";
-
-  rocprofiler_group_t& group = entry->group;
-  if (group.context == NULL) {
-    std::cout << "tool error: context is NULL\n";
-  }
-
-  rocprofiler_close(group.context);
+static void _atomic_store(volatile bool *flag, bool value)
+{
+#ifndef __STDC_NO_ATOMICS__
+    atomic_store((atomic_bool *) flag, value);
+#else
+#error "not atomics. brace yourselves!"
+    *flag = value;
+#endif
 }
 
-// Profiling completion handler
-// Dump and delete the context entry
-// Return true if the context was dumped successfully
-bool context_handler(rocprofiler_group_t group, void* arg) {
-  context_entry_t* entry = reinterpret_cast<context_entry_t*>(arg);
-
-  if (pthread_mutex_lock(&mutex) != 0) {
-    perror("pthread_mutex_lock");
-    abort();
-  }
-
-  dump_context_entry(entry);
-  delete entry;
-
-  if (pthread_mutex_unlock(&mutex) != 0) {
-    perror("pthread_mutex_unlock");
-    abort();
-  }
-
-  return false;
+static bool _atomic_load(const volatile bool *flag)
+{
+#ifndef __STDC_NO_ATOMICS__
+    return atomic_load((atomic_bool *) flag);
+#else
+#error "not atomics. brace yourselves!"
+    return *flag;
+#endif
 }
 
-// Kernel disoatch callback
-hsa_status_t dispatch_callback(const rocprofiler_callback_data_t* callback_data, void* /*user_data*/,
-                               rocprofiler_group_t* group) {
-    std::cout << "dispatch callback\n";
-  // HSA status
-  hsa_status_t status = HSA_STATUS_ERROR;
+static hsa_status_t _rocp_dispatch_callback(const rocprofiler_callback_data_t *callback_data,
+                                            void *arg, rocprofiler_group_t *group)
+{
+  std::cout << "dispatch\n";
+    hsa_agent_t agent = callback_data->agent;
+    hsa_status_t status = HSA_STATUS_ERROR;
 
-  // Profiling context
-  rocprofiler_t* context = NULL;
+    unsigned gpu_id = _get_gpu_id(agent);
+    assert(gpu_id >= 0);
 
-  // Context entry
-  context_entry_t* entry = new context_entry_t();
+    callback_arg_t *callback_arg = (callback_arg_t *) arg;
+    rocprofiler_pool_t *pool = callback_arg->pools[gpu_id];
+    rocprofiler_pool_entry_t pool_entry;
+    status = rocprofiler_pool_fetch(pool, &pool_entry);
+    assert(status == HSA_STATUS_SUCCESS);
 
-  // context properties
-  rocprofiler_properties_t properties{};
-  properties.handler = context_handler;
-  properties.handler_arg = (void*)entry;
+    rocprofiler_t *context = pool_entry.context;
+    context_entry_t *entry = (context_entry_t *) pool_entry.payload;
 
-  // Open profiling context
-  status = rocprofiler_open(callback_data->agent, NULL, 0,
-                            &context, ROCPROFILER_MODE_SINGLEGROUP, &properties);
-  check_status(status);
+    status = rocprofiler_get_group(context, 0, group);
+    assert(status == HSA_STATUS_SUCCESS);
 
-  // Get group[0]
-  status = rocprofiler_get_group(context, 0, group);
-  check_status(status);
+    entry->agent = agent;
+    entry->group = *group;
+    entry->data = *callback_data;
+    entry->data.kernel_name = strdup(callback_data->kernel_name);
+    _atomic_store(&entry->valid, true);
 
-  // Fill profiling context entry
-  entry->agent = callback_data->agent;
-  entry->group = *group;
-  entry->data = *callback_data;
-  entry->data.kernel_name = strdup(callback_data->kernel_name);
-  reinterpret_cast<std::atomic<bool>*>(&entry->valid)->store(true);
+    //fprintf(stdout, "%s tid(%ld)\n", __func__, syscall(SYS_gettid));
 
-  return HSA_STATUS_SUCCESS;
+    return status;
 }
 
+static void _dump_context_entry(context_entry_t *entry,
+                                rocprofiler_feature_t *features,
+                                unsigned feature_count)
+{
+    std::cout << "dumo_context_entry\n";
 
-/*hsa_status_t dispatch_callback(const rocprofiler_callback_data_t *callback_data,
-                               void *user_data, rocprofiler_group_t *group) {
+    while(_atomic_load(&entry->valid) == false) sched_yield();
 
-  hsa_status_t status = HSA_STATUS_SUCCESS;
+    fflush(stdout);
+    fprintf(stdout, "kernel symbol(0x%lx) name(\"%s\") tid(%u) queue-id(%u) "
+                    "gpu-id(%u) ",
+                    entry->data.kernel_object,
+                    entry->data.kernel_name,
+                    entry->data.thread_id,
+                    entry->data.queue_id,
+                    _get_gpu_id(entry->agent));
+    if (entry->data.record)
+        fprintf(stdout, "time(%lu,%lu,%lu,%lu)\n",
+                        entry->data.record->dispatch,
+                        entry->data.record->begin,
+                        entry->data.record->end,
+                        entry->data.record->complete);
+    fflush(stdout);
 
+    assert(entry->group.context != NULL);
 
-  // Profiling info objects
-  rocprofiler_feature_t features * = new rocprofiler_feature_t[2];
-  // Tracing parameters
-  rocprofiler_feature_parameter_t *parameters =
-      new rocprofiler_feature_parameter_t[2];
+    if (feature_count > 0) {
+        hsa_status_t status = rocprofiler_group_get_data(&entry->group);
+        assert(status == HSA_STATUS_SUCCESS);
+        status = rocprofiler_get_metrics(entry->group.context);
+        assert(status == HSA_STATUS_SUCCESS);
+    }
 
-  // Setting profiling features
-  features[0].type = ROCPROFILER_METRIC;
-  features[0].name = "L1_MISS_RATIO";
-  features[1].type = ROCPROFILER_METRIC;
-  features[1].name = "DRAM_BANDWIDTH";
+    for (unsigned i = 0; i < feature_count; ++i) {
+        const rocprofiler_feature_t *p = &features[i];
+        fprintf(stdout, "> %s ", p->name);
+        switch(p->data.kind) {
+            case ROCPROFILER_DATA_KIND_INT64:
+                fprintf(stdout, "= (%lu)\n", p->data.result_int64);
+                break;
+            default:
+                fprintf(stdout, "Undefined data kind(%u)\n", p->data.kind);
+                assert(0);
+        }
+    }
+}
 
-  // Creating profiling context
-  status = rocprofiler_open(callback_data->dispatch.agent, features, 2,
-                            &context, ROCPROFILER_MODE_SINGLEGROUP, NULL);
+static bool _rocp_context_handler(const rocprofiler_pool_entry_t *entry,
+                                  void *arg)
+{
+    context_entry_t *ctx_entry = (context_entry_t *) entry->payload;
+    handler_arg_t *handler_arg = (handler_arg_t *) arg;
 
-  return status;
-}*/
+    pthread_mutex_lock(&feature_lock);
 
+    //fprintf(stdout, "%s tid(%ld)\n", __func__, syscall(SYS_gettid));
+    _dump_context_entry(ctx_entry, handler_arg->features,
+                        handler_arg->feature_count);
 
+    pthread_mutex_unlock(&feature_lock);
 
+    return false;
+}
 
-void measureMetricInit(){};
+static hsa_status_t _count_devices(hsa_agent_t agent, void *data)
+{
+    unsigned *count = (unsigned *) data;
+    hsa_device_type_t type;
+    hsa_status_t status = hsa_agent_get_info(agent, HSA_AGENT_INFO_DEVICE, &type);
+    assert(status == HSA_STATUS_SUCCESS);
+    if (type == HSA_DEVICE_TYPE_GPU) {
+        agent_info_arr[(*count)++].agent = agent;
+    }
+    return status;
+}
+
+static unsigned _get_device_count(void)
+{
+    unsigned count = 0;
+    hsa_status_t status = hsa_iterate_agents(&_count_devices, &count);
+    assert(status == HSA_STATUS_SUCCESS);
+    return count;
+}
+
+static hsa_agent_t _get_agent(unsigned gpu_id)
+{
+    return agent_info_arr[gpu_id].agent;
+}
+
+static int init_intercept(rocprofiler_feature_t *features, unsigned feature_count)
+{
+    std::cout << "init intercept\n";
+    handler_arg = (handler_arg_t *)calloc(1, sizeof(*handler_arg));
+    assert(handler_arg != NULL);
+    handler_arg->features = features;
+    handler_arg->feature_count = feature_count;
+
+    rocprofiler_pool_properties_t properties;
+    properties.num_entries = 100;
+    properties.payload_bytes = sizeof(context_entry_t);
+    properties.handler = _rocp_context_handler;
+    properties.handler_arg = handler_arg;
+
+    unsigned gpu_count = _get_device_count();
+    agent_info_arr_len = gpu_count;
+
+    for (unsigned gpu_id = 0; gpu_id < gpu_count; ++gpu_id) {
+        hsa_agent_t agent = _get_agent(gpu_id);
+
+        rocprofiler_pool_t *pool = NULL;
+        hsa_status_t status = rocprofiler_pool_open(agent, features,
+                                                    feature_count, &pool,
+                                                    0, &properties);
+        assert(status == HSA_STATUS_SUCCESS);
+
+        callback_arg->pools[gpu_id] = pool;
+    }
+
+    rocprofiler_queue_callbacks_t callback_ptr;
+    callback_ptr.dispatch = _rocp_dispatch_callback;
+
+    hsa_status_t status = rocprofiler_set_queue_callbacks(callback_ptr,
+                                                          callback_arg);
+    assert(status == HSA_STATUS_SUCCESS);
+
+    return 0;
+}
+
+static int start_intercept(void)
+{
+    std::cout << "start intercept\n";
+    hsa_status_t status = rocprofiler_start_queue_callbacks();
+    assert(status == HSA_STATUS_SUCCESS);
+    return 0;
+}
+
+static int stop_intercept(void)
+{
+    std::cout << "stop intercept\n";
+    hsa_status_t status = rocprofiler_stop_queue_callbacks();
+    assert(status == HSA_STATUS_SUCCESS);
+    return 0;
+}
+
+static int shutdown_intercept(void)
+{
+    std::cout << "shutdown intercept\n";
+    hsa_status_t status = rocprofiler_remove_queue_callbacks();
+    assert(status == HSA_STATUS_SUCCESS);
+
+    int i;
+    //for (i = 0; i < agent_info_arr_len; ++i) {
+    //    status = rocprofiler_pool_flush(callback_arg->pools[i]);
+    //    assert(status == HSA_STATUS_SUCCESS);
+    //    status = rocprofiler_pool_close(callback_arg->pools[i]);
+    //    assert(status == HSA_STATUS_SUCCESS);
+    //}
+    ////free(handler_arg);
+    return 0;
+}
 void measureBandwidthStart() {
-    std::cout << "measureBandwidthStart\n";
-    rocprofiler_queue_callbacks_t callbacks_ptrs{};
-    callbacks_ptrs.dispatch = dispatch_callback;
-    check_status( rocprofiler_set_queue_callbacks(callbacks_ptrs, NULL) );
-    check_status( rocprofiler_start_queue_callbacks() );
+    start_intercept();
 
-};
+}
 
 std::vector<double> measureMetricStop() {
+    hipDeviceSynchronize();
+    stop_intercept();
+    return std::vector<double>(6, 0);
 
-    std::cout << "measureBandwidthStop\n";
-    rocprofiler_stop_queue_callbacks();
-
-
-
-    return std::vector<double>(6, 0.0);
 }
+
+void initMeasureMetric() {
+    char*rocm_root = "/opt/rocm/";
+
+    char metrics_path[128];
+  sprintf(metrics_path, "%s/%s", rocm_root, "rocprofiler/lib/metrics.xml");
+  //setenv("ROCP_TOOL_LIB", "/opt/rocm/rocprofiler/tool/libtool.so", 1);
+  setenv("ROCP_METRICS", metrics_path, 1);
+  setenv("ROCP_HSA_INTERCEPT", "1", 1);
+  setenv("ROCPROFILER_LOG", "1", 1);
+  setenv("HSA_TOOLS_LIB", "librocprofiler64.so", 1);
+  setenv("HSA_TOOLS_LIB", "librocprofiler64.so", 1);
+
+
+  hipDeviceProp_t devProp;
+  hipGetDeviceProperties(&devProp, 0);
+  std::cout << " System minor " << devProp.minor << std::endl;
+  std::cout << " System major " << devProp.major << std::endl;
+  std::cout << " agent prop name " << devProp.name << std::endl;
+
+  std::cout << "hip Device prop succeeded " << std::endl ;
+  rocprofiler_feature_t features[4];
+  features[0].kind = ROCPROFILER_FEATURE_KIND_METRIC;
+  features[0].name = "SQ_WAVES";
+  features[1].kind = ROCPROFILER_FEATURE_KIND_METRIC;
+  features[1].name = "SQ_INSTS_VALU";
+
+  unsigned feature_count = 2;
+  init_intercept(features, feature_count);
+}
+
 
 #endif // ROCM-METRICS_H_
