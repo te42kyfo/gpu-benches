15f814ad89275a656b1e3b6148d17fa80c7b220a
Dominik Ernst <dominik.ernst@fau.de>
2024-04-09 17:46:38 +0200

    gpu-strides: unify data output and benchmarks for machine readability

--- gpu-strides/main.cu
+++ gpu-strides/main.cu
@@ -34,54 +34,23 @@ __global__ void initKernel(T *A, const T *__restrict__ B, const size_t N) {
   }
 }
 
-/*float/double
-read/write
-alignment
-
-uniform
-unit stride
-stride
-block stride
-
-single block
-*/
-
 template <typename T, int N>
-__global__ void uniformKernel(T *A, T *B, int zero, int alignment) {
-  int tidx = threadIdx.x;
+__global__ __launch_bounds__(1024) void blockKernel(T *A, T *B, int zero,
+                                                    int pitch) {
+  int tidx = threadIdx.x % 64;
+  int tidy = threadIdx.y % 32;
 
   T sum = T(0);
-  T *A2 = A + tidx * zero + alignment;
+  T *A2 = A + tidx + tidy * pitch + pitch;
 
-  for (int n = 0; n < N; n += 10) {
-    if (sum == 1232)
-      A2 += zero * n;
-    sum += A2[1 * 32] * A2[2 * 32];
-    sum += A2[4 * 32] * A2[3 * 32];
-    sum += A2[5 * 32] * A2[6 * 32];
-    sum += A2[8 * 32] * A2[7 * 32];
-    sum += A2[9 * 32] * A2[10 * 32];
-  }
-  if (sum == T(123123.23))
-    B[tidx] = sum;
-}
-
-template <typename T, int N>
-__global__ void blockKernel(T *A, T *B, int zero, int pitch) {
-  int tidx = threadIdx.x;
-  int tidy = threadIdx.y % 64;
-
-  T sum = T(0);
-  T *A2 = A + tidx + tidy * pitch;
 #pragma unroll 1
   for (int n = 0; n < N; n += 8) {
     A2 += zero;
-    sum += A2[8] * A2[16];
-    sum += A2[48] * A2[32];
-    sum += A2[64] * A2[96];
-    sum += A2[128] * A2[0];
+    sum += A2[0] * A2[4];
+    sum += A2[8] * A2[12];
+    sum += A2[20] * A2[16];
+    sum += A2[24] * A2[28];
   }
-
   if (sum == T(123123.23))
     B[tidx] = sum;
 }
@@ -91,34 +60,23 @@ __global__ void strideKernel(T *A, T *B, int zero, int stride) {
   int tidx = threadIdx.x;
 
   T sum = T(0);
-  T *A2 = A + tidx * stride;
+  T *A2 = A + (tidx % 64) * stride;
 
+#pragma unroll 1
   for (int n = 0; n < N; n += 8) {
     A2 += zero;
-    sum += A2[0] * A2[1];
-    sum += A2[2] * A2[3];
-    sum += A2[4] * A2[5];
-    sum += A2[6] * A2[7];
-  }
+    sum += A2[0] * A2[4];
+    sum += A2[8] * A2[12];
+    sum += A2[20] * A2[16];
+    sum += A2[24] * A2[28];
 
-  if (sum == T(123123.23))
-    B[tidx] = sum;
-}
+    // T v3 = A2[17] + A2[20] * A2[22];
+    // T v4 = A2[24] + A2[26] * A2[30];
+    // T v5 = A2[32] + A2[35] * A2[38];
+    // T v6 = A2[40] + A2[43] * A2[45];
 
-template <typename T, int N>
-__global__ void stencilKernel(T *A, T *B, int zero, int pitch) {
-  int tidx = threadIdx.x;
-  int tidy = threadIdx.y % 64;
-
-  T sum = T(0);
-  T *A2 = A + tidx + tidy * pitch + pitch;
-
-  // #pragma unroll 1
-  for (int n = 0; n < N; n += 2) {
-    A2 += zero;
-    sum += A2[0] * A2[4]; //  + A+ A2[-1 - pitch] + A2[0 - pitch] +
-                          // A2[1 - pitch] + A2[-1 + pitch] + A2[0 +
-                          // pitch] + A2[1 + pitch];
+    // v1 = v1 + v2 * v3;
+    // v5 = v4 + v5 * v6;
   }
 
   if (sum == T(123123.23))
@@ -127,12 +85,12 @@ __global__ void stencilKernel(T *A, T *B, int zero, int pitch) {
 
 #ifdef __HIPCC__
 std::vector<const char *> metrics = {
-    //"TA_TA_BUSY_sum", "TA_TOTAL_WAVEFRONTS_sum",
-    //"TA_UTIL", "TA_BUSY_avr"
-    // "TA_ADDR_STALLED_BY_TC_CYCLES_sum",
-    // "TA_ADDR_STALLED_BY_TD_CYCLES_sum",
-    // "TA_DATA_STALLED_BY_TC_CYCLES_sum",
-    // "TA_FLAT_WAVEFRONTS_sum",
+    "ZERO", "ZERO" //, "GL2C_HIT_sum", "GL2C_MISS_sum"
+                   //"TA_TOTAL_WAVEFRONTS_sum",
+                   //"TA_UTIL", "TA_BUSY_avr"
+                   // "TA_ADDR_STALLED_BY_TC_CYCLES_sum",
+                   // "TA_ADDR_STALLED_BY_TD_CYCLES_sum",
+    //"TA_DATA_STALLED_BY_TC_CYCLES_sum", "TA_FLAT_WAVEFRONTS_sum",
     //"TA_FLAT_READ_WAVEFRONTS_sum", "TA_BUFFER_WAVEFRONTS_sum",
     // "TA_BUFFER_READ_WAVEFRONTS_sum",
     // "TA_BUFFER_WRITE_WAVEFRONTS_sum",
@@ -157,11 +115,10 @@ std::vector<const char *> metrics = {
     //"TCP_UTCL1_PERMISSION_MISS_sum",
     // "TCP_TOTAL_CACHE_ACCESSES_sum",
     // "TCP_TCP_LATENCY_sum",
-    // "TCP_TA_TCP_STATE_READ_sum",
+    //"TCP_TA_TCP_STATE_READ_sum",
     // "TCP_TCC_READ_REQ_LATENCY_sum",
     // "TCP_TCC_WRITE_REQ_LATENCY_sum",
-    //"TCP_TCC_READ_REQ_sum"
-    //"TCP_TCC_WRITE_REQ_sum"
+    // "TCP_TCC_READ_REQ_sum", "TCP_TCC_WRITE_REQ_sum",
     // "TCP_TCC_NC_READ_REQ_sum",
     // "TCP_TCC_UC_READ_REQ_sum",
     // "TCP_TCC_CC_READ_REQ_sum",
@@ -170,7 +127,8 @@ std::vector<const char *> metrics = {
 };
 #else
 std::vector<const char *> metrics = {
-    "l1tex__cycles_active.sum", "l1tex__data_pipe_lsu_wavefronts.sum",
+    //"l1tex__cycles_active.sum",
+    "l1tex__data_pipe_lsu_wavefronts.sum",
     //"l1tex__data_pipe_tex_wavefronts.sum",
     "l1tex__t_output_wavefronts_pipe_lsu_mem_global_op_ld.sum",
     //"l1tex__t_sectors_pipe_lsu_mem_global_op_ld_lookup_hit.sum",
@@ -180,9 +138,10 @@ std::vector<const char *> metrics = {
     //"l1tex__tex_writeback_active.sum",
     //"l1tex__lsuin_requests.sum", "l1tex__data_bank_reads.sum",
     //"l1tex__data_bank_writes.sum",
-    "l1tex__m_l1tex2xbar_req_cycles_active.sum",
-    "l1tex__m_xbar2l1tex_read_sectors.sum",
-    "l1tex__texin_sm2tex_req_cycles_active.sum", "lts__t_sectors.sum"};
+    //"l1tex__m_l1tex2xbar_req_cycles_active.sum",
+    //"l1tex__m_xbar2l1tex_read_sectors.sum",
+    //"l1tex__texin_sm2tex_req_cycles_active.sum",
+    "lts__t_sectors.sum"};
 #endif
 
 template <typename T, int N>
@@ -193,7 +152,7 @@ std::vector<double> measureFunc(kernel_ptr_type<T, N> kernel, dim3 blockSize,
   cudaDeviceProp prop;
   GPU_ERROR(cudaGetDevice(&deviceId));
   GPU_ERROR(cudaGetDeviceProperties(&prop, deviceId));
-  int warpSize = prop.warpSize;
+  int warpSize = 32; // prop.warpSize;  // normalize to 32 for all GPU types
 
   std::vector<double> results;
 
@@ -210,21 +169,22 @@ std::vector<double> measureFunc(kernel_ptr_type<T, N> kernel, dim3 blockSize,
     GPU_ERROR(cudaDeviceSynchronize());
 
     kernel<<<grid, blockSize>>>((T *)dA, (T *)dB, 0, arg);
-    kernel<<<grid, blockSize>>>((T *)dA, (T *)dB, 0, arg);
-    kernel<<<grid, blockSize>>>((T *)dA, (T *)dB, 0, arg);
-    kernel<<<grid, blockSize>>>((T *)dA, (T *)dB, 0, arg);
     GPU_ERROR(cudaDeviceSynchronize());
     double t2 = dtime();
-    double dt = (t2 - t1) / 4;
+    double dt = (t2 - t1);
     time.add(dt);
   }
 
   results.push_back(time.minValue() * gpu_clock * 1e6 /
-                    (blockSize.x * blockSize.y * N / warpSize));
+                    ((size_t)blockSize.x * blockSize.y * N / warpSize));
 
-  results.push_back((blockSize.x * blockSize.y * N * sizeof(T)) /
+  results.push_back(((size_t)blockSize.x * blockSize.y * N * sizeof(T)) /
                     (time.minValue() * gpu_clock * 1e6));
   for (auto metricName : metrics) {
+    if (metricName == "ZERO") {
+      results.push_back(0);
+      continue;
+    }
     MeasurementSeries metricSeries;
     GPU_ERROR(cudaDeviceSynchronize());
     measureMetricsStart({metricName});
@@ -257,6 +217,16 @@ dim3 getColor(double intensity) {
               max(0.0, min(1.0, 2.4 - intensity * 2.0)) * 255);
 }
 
+int getPrecision(double value) {
+
+  int precision = 1;
+  if (value > 50)
+    precision = 0;
+  if (value < 5.0)
+    precision = 2;
+  return precision;
+}
+
 void format(std::vector<std::vector<double>> values,
             std::vector<string> rowLabels) {
 
@@ -274,11 +244,8 @@ void format(std::vector<std::vector<double>> values,
       if (value < rowMin[row] || rowMin[row] == -1)
         rowMin[row] = value;
 
-      int precision = 1;
-      if (value > 50)
-        precision = 0;
-      if (value < 5.0)
-        precision = 2;
+      int precision = getPrecision(value);
+
       stringstream buf;
       buf << fixed << setprecision(precision) << v[row];
       strings[row].push_back(buf.str());
@@ -341,71 +308,58 @@ int main(int argc, char **argv) {
     rowLabels.push_back(metricName);
   }
 
-  std::cout << "\nfloat stride\n";
-  for (int i = 0; i < 16; i++) {
-    values.push_back(measureFunc<float, 10000>(strideKernel<float, 10000>,
-                                               dim3(1024, 1, 1), i));
-    values.back().insert(begin(values.back()), i);
-  }
-  format(values, rowLabels);
-  values.clear();
+  const int N = 18 * 64 * 1024;
 
-  std::cout << "\ndouble stride\n";
-  for (int i = 0; i < 16; i++) {
-    values.push_back(measureFunc<double, 10000>(strideKernel<double, 10000>,
-                                                dim3(1024, 1, 1), i));
-    values.back().insert(begin(values.back()), i);
-  }
+  cout << "\n";
 
-  format(values, rowLabels);
-  values.clear();
-  std::cout << "\nuniform double\n";
-  values.clear();
-  for (int i = 0; i < 4; i++) {
-    values.push_back(measureFunc<double, 10000>(uniformKernel<double, 10000>,
-                                                dim3(1024, 1, 1), i));
-    values.back().insert(begin(values.back()), i);
-  }
-
-  format(values, rowLabels);
-  values.clear();
-
-  std::cout << "\nuniform float\n";
-  for (int i = 0; i < 4; i++) {
-    values.push_back(measureFunc<float, 10000>(uniformKernel<float, 10000>,
-                                               dim3(1024, 1, 1), i));
-    values.back().insert(begin(values.back()), i);
-  }
-
-  format(values, rowLabels);
-  values.clear();
-
-  std::cout << "\nfloat block strides:\n";
-  for (int i = 1; i <= 64; i *= 2) {
-    values.clear();
-    for (int pitch = 0; pitch <= 16; pitch++) {
-      values.push_back(measureFunc<float, 10000>(
-          stencilKernel<float, 10000>, dim3(i, 1024 / i, 1), 4096 + pitch));
-      values.back().insert(begin(values.back()), pitch);
+  for (int i = 0; i <= 64; i++) {
+    auto values =
+        measureFunc<float, N>(strideKernel<float, N>, dim3(1024, 1, 1), i);
+    cout << "float stride   " << setw(3) << i << "  " << setw(3) << i << "  ";
+    for (auto v : values) {
+      cout << fixed << setprecision(getPrecision(v)) << setw(6) << v << " ";
     }
-    std::cout << i << "\n";
-    format(values, rowLabels);
+    cout << "\n";
   }
+  cout << "\n";
 
-  std::cout << "\ndouble block strides:\n";
-  for (int i = 1; i <= 64; i *= 2) {
-    values.clear();
-    for (int pitch = 0; pitch <= 16; pitch++) {
-      values.push_back(measureFunc<double, 10000>(
-          stencilKernel<double, 10000>, dim3(i, 1024 / i, 1), 4096 + pitch));
-      values.back().insert(begin(values.back()), pitch);
+  for (int i = 0; i <= 64; i++) {
+    auto values =
+        measureFunc<double, N>(strideKernel<double, N>, dim3(1024, 1, 1), i);
+    cout << "double stride   " << setw(3) << i << "  " << setw(3) << i << "  ";
+    for (auto v : values) {
+      cout << fixed << setprecision(getPrecision(v)) << setw(6) << v << " ";
     }
-    std::cout << i << "\n";
-    format(values, rowLabels);
+    cout << "\n";
   }
-  values.clear();
+  cout << "\n";
+  for (int w = 1; w <= 64; w *= 2) {
+    int pitch = 4096 + 2;
+    auto values = measureFunc<float, N>(blockKernel<float, N>,
+                                        dim3(w, 1024 / w, 1), pitch);
 
-  std::cout << "\n";
+    cout << "float block   " << setw(3) << w << "  " << setw(3) << pitch
+         << "  ";
+    for (auto v : values) {
+      cout << fixed << setprecision(getPrecision(v)) << setw(6) << v << " ";
+    }
+    cout << "\n";
+  }
+
+  cout << "\n";
+  for (int w = 1; w <= 64; w *= 2) {
+    int pitch = 4096 + 2;
+    auto values = measureFunc<double, N>(blockKernel<double, N>,
+                                         dim3(w, 1024 / w, 1), pitch);
+
+    cout << "double block   " << setw(3) << w << "  " << setw(3) << pitch
+         << "  ";
+    for (auto v : values) {
+      cout << fixed << setprecision(getPrecision(v)) << setw(6) << v << " ";
+    }
+    cout << "\n";
+  }
+
   GPU_ERROR(cudaFree(dA));
   GPU_ERROR(cudaFree(dB));
 }
